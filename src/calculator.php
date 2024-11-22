863bc894677e78667db4eb1cf0953a82ea5aa3bb
YimoEx <159985352+yimoex@users.noreply.github.com>
2024-11-22 17:25:29 +0800

    support update
    添加对 PI 和负数的支持

--- src/calculator.php
+++ src/calculator.php
@@ -10,33 +10,41 @@ class Calculator {
         $length = strlen($string);
         $buf = '';
         $last = 0; //0表示操作符,1为值
+        $signal = 1; // -1 or 1表达正负
         do{
             $t = $string[$ptr];
             if($t == ' '){
                 $ptr++;
                 continue;
             }
-            if($t === '+' || $t === '-'){
+            if($t === '+' || ($t === '-' && $signal === -1)){
                 if($buf == NULL || $last === 1) $this -> error('错误的表达式类型');
-                $node = $node -> add('val', PRIORITY_VAL, $buf)
+                $node = $node -> add('val', PRIORITY_VAL, $signal * $buf)
                               -> add($t, PRIORITY_PS);
                 $buf = '';
                 $last = 1;
+                $signal = 1;
             }else if($t === '*' || $t === '/' || $t === '%'){
                 if($buf == NULL || $last === 1) $this -> error('错误的表达式类型');
-                $node = $node -> add('val', PRIORITY_VAL, $buf)
+                $node = $node -> add('val', PRIORITY_VAL, $signal * $buf)
                               -> add($t, PRIORITY_MD);
                 $buf = '';
                 $last = 1;
+                $signal = 1;
             }else if($t === '^'){
                 if($buf == NULL || $last === 1) $this -> error('错误的表达式类型');
-                $node = $node -> add('val', PRIORITY_VAL, $buf)
+                $node = $node -> add('val', PRIORITY_VAL, $signal * $buf)
                               -> add($t, PRIORITY_POW);
                 $buf = '';
                 $last = 1;
+                $signal = 1;
             }else{
                 if($t === 'e'){
                     $buf = (string)M_E;
+                }else if($t === 'p'){
+                    $buf = (string)M_PI;
+                }else if($t === '-'){
+                    $signal = -1;
                 }if(!is_numeric($t)){
                     if($t !== '.'){
                         $this -> error('错误的表达式字符 => ' . $t);
